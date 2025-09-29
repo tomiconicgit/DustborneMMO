a60fb3cc86f3c3481733dd238a0d001bd8654987
tomiconicgit <tomiconicx@icloud.com>
2025-09-29 21:26:22 +0100

    Create WorldMap.js

---
+++ src/game/world/WorldMap.js
@@ -0,0 +1,8 @@
+// file: src/game/world/WorldMap.js
+
+// The dimensions of the flat world plane in tiles.
+export const WORLD_WIDTH = 30;
+export const WORLD_DEPTH = 30;
+
+// The size of a single tile in world units.
+export const TILE_SIZE = 1;
