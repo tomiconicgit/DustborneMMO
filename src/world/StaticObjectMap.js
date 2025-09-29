8e196c2e4e1bc7e6c57265b7225cd487f249bd38
tomiconicgit <tomiconicx@icloud.com>
2025-09-29 21:26:30 +0100

    Delete src/world directory

--- src/world/StaticObjectMap.js
+++
@@ -1,9 +0,0 @@
-// file: src/game/world/StaticObjectMap.js
-
-// This map will hold hand-placed objects in the world.
-// It is currently empty for this fresh start.
-// Example format:
-// export const STATIC_OBJECTS = {
-//   "0,0": [ { type: 'rock', x: 10, z: 12 } ]
-// };
-export const STATIC_OBJECTS = {};
