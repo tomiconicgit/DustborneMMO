98d58aaf3300b80f8c9b798338329e63aa7bb6b0
tomiconicgit <tomiconicx@icloud.com>
2025-09-29 20:29:21 +0100

    Create StaticObjectMap.js

---
+++ src/world/StaticObjectMap.js
@@ -0,0 +1,9 @@
+// file: src/game/world/StaticObjectMap.js
+
+// This map will hold hand-placed objects in the world.
+// It is currently empty for this fresh start.
+// Example format:
+// export const STATIC_OBJECTS = {
+//   "0,0": [ { type: 'rock', x: 10, z: 12 } ]
+// };
+export const STATIC_OBJECTS = {};
